8158e887ec225a047ee1862ff5d65ac76a66d0ce
William Perez <wil.a.zz261102@gmail.com>
2026-09-01 12:17:30 -0500

    feat: allow Capacitor frontend origin

--- Program.cs
+++ Program.cs
@@ -70,7 +70,11 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Frontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:8100")
+            .WithOrigins(
+                "http://localhost:8100",
+                "http://localhost",
+                "https://localhost"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
